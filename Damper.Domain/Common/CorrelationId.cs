4bb5bae85f9fd9f6a1c00bf5d71edede4a45ec2f
Roy Chancellor <roychance600@gmail.com>
2026-08-04 14:54:51 -0700

    Added parameterless constructors that call the pirimary constructor with string.Empty to prevent null values when simply calling "new()".

--- Damper.Domain/Common/CorrelationId.cs
+++ Damper.Domain/Common/CorrelationId.cs
@@ -2,6 +2,8 @@ namespace Damper.Domain.Common
 {
     public readonly record struct CorrelationId(string Value)
     {
+        public CorrelationId() : this(string.Empty) { }
+        
         public override string ToString() => Value;
     }
 }
--- Damper.Domain/Common/IntegrationName.cs
+++ Damper.Domain/Common/IntegrationName.cs
@@ -2,6 +2,8 @@ namespace Damper.Domain.Common
 {
     public readonly record struct IntegrationName(string Value)
     {
+        public IntegrationName() : this(string.Empty) { }
+        
         public override string ToString() => Value;
     }
 }
